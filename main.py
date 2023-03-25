baf4ca224543d0e0b701c86ea9bbe2fe3d4ff93c
Евгений Коршняков <e.korshnyakov@gmail.com>
2023-03-25 22:39:17 +0600

    code according to PEP8

--- main.py
+++ main.py
@@ -3,25 +3,35 @@ from random import randint
 
 def attack(char_name, char_class):
     if char_class == 'warrior':
-        return (f'{char_name} нанёс урон противнику равный {5 + randint(3, 5)}')
+        damage = 5 + randint(3, 5)
+        return (f'{char_name} нанёс урон противнику равный {damage}')
     if char_class == 'mage':
-        return (f'{char_name} нанёс урон противнику равный {5 + randint(5, 10)}')
+        damage = 5 + randint(5, 10)
+        return (f'{char_name} нанёс урон противнику равный {damage}')
     if char_class == 'healer':
-        return (f'{char_name} нанёс урон противнику равный {5 + randint(-3, -1)}')
+        damage = 5 + randint(5, 10)
+        return (f'{char_name} нанёс урон противнику равный {damage}')
+    
+
 def defence(char_name, char_class):
     if char_class == 'warrior':
-        return (f'{char_name} блокировал {10 + randint(5, 10)} урона')
+        defence_char = 10 + randint(5, 10)
+        return (f'{char_name} блокировал {defence_char} урона')
     if char_class == 'mage':
-        return (f'{char_name} блокировал {10 + randint(-2, 2)} урона')
+        defence_char = 10 + randint(-2, 2)
+        return (f'{char_name} блокировал {defence_char} урона')
     if char_class == 'healer':
-        return (f'{char_name} блокировал {10 + randint(2, 5)} урона')
+       defence_char = 10 + randint(2, 5)
+        return (f'{char_name} блокировал {defence_char} урона')
+
+
 def special(char_name, char_class):
     if char_class == 'warrior': 
-        return (f'{char_name} применил специальное умение «Выносливость {80 + 25}»')
+        return (f'{char_name} применил специальное умение «Выносливость {80 + 25}')
     if char_class == 'mage':
-        return (f'{char_name} применил специальное умение «Атака {5 + 40}»')
+        return (f'{char_name} применил специальное умение «Атака {5 + 40}')
     if char_class == 'healer':
-        return (f'{char_name} применил специальное умение «Защита {10 + 30}»')
+        return (f'{char_name} применил специальное умение «Защита {10 + 30}')
 
 
 
